9eeb834196716b1dec2d55389a8e98f96baaf96e
thinhdaotrong <thinh.daotrong178@gmail.com>
2021-10-19 10:09:14 +0700

    first commit

--- src/helpers/faceApi.js
+++ src/helpers/faceApi.js
@@ -1,4 +1,4 @@
-import * as faceapi from "face-api.js";
+import * as faceapi from 'face-api.js';
 
 export const loadModels = () => {
   const MODEL_URL = `${process.env.PUBLIC_URL}/models`;
@@ -12,22 +12,19 @@ export const loadModels = () => {
   ]);
 };
 
-export const detectFaces = async image => {
+export const detectFaces = async (image) => {
   if (!image) {
     return;
   }
 
   const imgSize = image.getBoundingClientRect();
-  const displaySize = {width: imgSize.width, height: imgSize.height};
+  const displaySize = { width: imgSize.width, height: imgSize.height };
   if (displaySize.height === 0) {
     return;
   }
 
   const faces = await faceapi
-    .detectAllFaces(
-      image,
-      new faceapi.TinyFaceDetectorOptions({inputSize: 320})
-    )
+    .detectSingleFace(image, new faceapi.TinyFaceDetectorOptions({ inputSize: 320 }))
     .withFaceLandmarks()
     .withFaceExpressions()
     .withAgeAndGender();
@@ -38,21 +35,21 @@ export const detectFaces = async image => {
 export const drawResults = async (image, canvas, results, type) => {
   if (image && canvas && results) {
     const imgSize = image.getBoundingClientRect();
-    const displaySize = {width: imgSize.width, height: imgSize.height};
+    const displaySize = { width: imgSize.width, height: imgSize.height };
     faceapi.matchDimensions(canvas, displaySize);
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     const resizedDetections = faceapi.resizeResults(results, displaySize);
     switch (type) {
-      case "landmarks":
+      case 'landmarks':
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         break;
-      case "expressions":
+      case 'expressions':
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
         break;
-      case "box":
+      case 'box':
         faceapi.draw.drawDetections(canvas, resizedDetections);
         break;
-      case "boxLandmarks":
+      case 'boxLandmarks':
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         break;
